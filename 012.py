fb588e977e4d5e967d847cb3d1930130dc7d54f1
ismycris <cristielythainara@gmail.com>
2024-04-25 15:17:39 -0300

    update

--- 012.py
+++ 012.py
@@ -1,1 +1,10 @@
-nome = str(input('qual seu nome'))
+nome = str(input('qual seu nome?'))
+if nome == 'gustavo':
+    print('que nome bonito!')
+elif nome == 'pedro'or nome=='maria' or nome == 'paula':
+    print('seu nome  bem popular no brasil')
+elif nome in 'ana claudia jessica juliana':
+    print('belo nome')
+else:
+    print('seu nome e bem normal.')
+print('tenha um bom dia, {}!'.format(nome))
---
+++ desafio036.py
@@ -0,0 +1,9 @@
+# programa para aprovar o emprestimo bancario para a compra de uma casa
+# o programa deve perguntar o valor da casa, salario e em quantos anos vai pagar
+#calcule o valor da prestacao mensal sabendo que nao pode exeder 30% do salario 
+
+casa = str(input('qual o valor da casa?'))
+salario = str(input('valor do salario?'))
+parcelas = str(input('em quantos anoa pretende pagar?'))
+
+
