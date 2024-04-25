80d8faad74d2a67542107cd7c4baad7dc5c866af
ismycris <cristielythainara@gmail.com>
2024-04-25 16:18:28 -0300

    update

--- desafio036.py
+++ desafio036.py
@@ -2,8 +2,16 @@
 # o programa deve perguntar o valor da casa, salario e em quantos anos vai pagar
 #calcule o valor da prestacao mensal sabendo que nao pode exeder 30% do salario 
 
-casa = str(input('qual o valor da casa?'))
-salario = str(input('valor do salario?'))
-parcelas = str(input('em quantos anoa pretende pagar?'))
+casa = float(input('qual o valor da casa: R$'))
+salario = float(input('valor do salario: R$'))
+anos = int(input('quantos anos de financiamento: R$'))
 
+prestacao =casa / (anos * 12)
+minimo = salario * 30 / 100
+if prestacao <= minimo:
+    print('emprestimo pode ser concedido')
+else:
+    print('emprestimo negado!')
 
+print('para pagar uma casa de R${:.2f} em {} anos a prestação sera de R${:.2f}'.format(casa, anos, prestacao))
+print('comparando tem que pagar {} e o minimo e de {}'.format(prestacao,minimo))
---
+++ desafio037.py
@@ -0,0 +1,4 @@
+#programa que leia um numero qualquer e peça para o usuario escolher qual sera a base da conversão
+#1 para binario
+#2 para octal
+#3 para hexadecimal
