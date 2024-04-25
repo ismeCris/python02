45e31f120d0cb8cc17a4b7236263a696d1ca4d02
ismycris <cristielythainara@gmail.com>
2024-04-25 16:46:22 -0300

    update

--- desafio037.py
+++ desafio037.py
@@ -2,3 +2,20 @@
 #1 para binario
 #2 para octal
 #3 para hexadecimal
+num = int(input('digite um numero inteiro:'))
+print('''escolha uma base para conversão:
+    [1]converter para BINARIO
+    [2]converter para OCTAL
+    [3]converter para HEXADECIMAL''')
+
+opcao =int(input('sua opção:'))
+
+if opcao ==1:
+    print('{}convertido para BINARIO e igual a {}'.format(num, bin(num)[2:]))
+elif opcao == 2:
+    print('{} convertido para OCTAL e igual a {}'.format(num, oct(num)[2:]))
+elif opcao == 3:
+    print('{} convertido para HEXADECIMAL e igual a {}'.format(num, hex(num)[2:]))
+else:
+    print('opção invalida')
+    
