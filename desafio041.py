6cecf417d46754456077c859561ac08ca637cc42
ismycris <cristielythainara@gmail.com>
2024-04-25 23:16:03 -0300

    update

--- desafio041.py
+++ desafio041.py
@@ -4,3 +4,21 @@
 # Até 19 anos: JÚNIOR
 # Até 25 anos: SÊNIOR
 # Acima de 25 anos: MASTER
+from datetime import date
+atual = date.today().year
+
+nasc = int(input('ano de nascimento: '))
+idade = atual - nasc
+
+print('o atleta tem {} anos'.format(nasc, idade))
+
+if idade <= 9:
+    print("categoria MIRIM")
+elif  idade >9 and idade <= 14:
+    print('categoria INFANTIL')
+elif  idade >14 and idade <= 19:
+    print('categoria JÚNIOR')
+elif  idade >19 and idade <= 25:
+    print('categoria SÊNIOR')
+else:
+    print('categoria MASTER')
