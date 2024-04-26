df14b0ad9fdc8cad34dffc172c2699f2d85fdc6b
ismycris <cristielythainara@gmail.com>
2024-04-26 16:17:31 -0300

    update

--- desafio044.py
+++ desafio044.py
@@ -4,3 +4,34 @@
 # em até 2x no cartão: preço formal
 # 3x ou mais no cartão: 20% de juros
 
+print('======== Lojas Cristina ========')
+preco=float(input('preço das compras: R$'))
+
+print('''FORMAS DE PAGAMENTO
+    [1] á vista dinheiro/cheque
+    [2] á vista cartão
+    [3] 2x ou mais no cartão
+    [4] 3x ou mais no cartão
+    ''')
+opcao = int(input('qual sua opção? '))
+
+if opcao == 1:
+    desconto = preco * (10/100)
+    final = preco - desconto
+    
+    print('sua compra de R${:.2f} vai ficar R${:.2f} no final'.format(preco, final))
+elif opcao == 2:
+    desconto = preco * (5/100)
+    final = preco - desconto
+    print('sua compra de R${:.2f} vai ficar R${:.2f} no final'.format(preco, final))
+elif opcao == 3:
+    print('sua compra  vai ficar R${:.2f} '.format(preco))
+elif opcao == 4:
+    parcelas = int(input('quantas parcelas?'))
+    juros = preco * (20/100)
+    final= preco + juros
+    mensal = final / parcelas
+    print('sua compra sera parcelada em R${:.2f} de R${:.2f} com juros'.format(parcelas, mensal))
+    print('sua compra de R${:.2f} vai ficar R${:.2f} no final'.format(preco, final))
+else:
+    print('opção invalida')
