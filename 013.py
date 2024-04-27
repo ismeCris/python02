53e294b2c2585299df58dff9d91be25f4cf5e06e
ismycris <cristielythainara@gmail.com>
2024-04-27 15:51:34 -0300

    update

---
+++ 013.py
@@ -0,0 +1,15 @@
+for c in range(0,7,2):
+    print(c)
+print('fim')
+
+n =int(input('digite um numero'))
+for c in range(0,n+1):
+    print(c)
+print('fim')
+
+i =int(input('inicio'))
+f =int(input('fim'))
+p =int(input('passo'))
+for c in range(i,f+1, p):
+    print(c)
+print('fim')
